6eccf54bdcbd063f2a6a02113788f875b02642fe
dannyvassallo <danielvassallo87@gmail.com>
2016-06-28 11:52:16 -0400

    redirect to login when not admin

--- app/controllers/rails_db/dashboard_controller.rb
+++ app/controllers/rails_db/dashboard_controller.rb
@@ -4,10 +4,6 @@ module RailsDb
     include CamaleonCms::SiteHelper
     include CamaleonCms::SessionHelper
 
-    def index
-      puts "FUCK #{current_user.role}", session.inspect
-    end
-
     def current_user
       return @cama_current_user if defined?(@cama_current_user)
       # api current user...
@@ -27,6 +23,13 @@ module RailsDb
     # calculate the current user for API
     def cama_calc_api_current_user
       current_site.users_include_admins.find(doorkeeper_token.resource_owner_id).decorate if doorkeeper_token rescue nil
+    end
+
+    def verify_access
+      result = RailsDb.verify_access_proc.call(self)
+      if !current_user || current_user.role != 'admin'
+        redirect_to '/admin'
+      end
     end
 
   end
